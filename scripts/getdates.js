8fc9e39c4b1ec094c4427963722c5f28a3b66baa
cynfonga <cynfonga@gmail.com>
2024-02-09 05:49:42 +0200

    init

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,9 @@ span1.textContent=new Date().toLocaleDateString('en-Us', options);
 
 const span2= document.getElementById('lastmodification');
 span2.textContent= document.lastModified;
+
+const button = document.querySelector("#hamburgermenu");
+const  list = document.querySelector("#hamburgerlist");
+button.addEventListener( 'click', function(){
+list.classList.toggle("open");
+  button.classList.toggle("open");});
