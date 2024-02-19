300ed76f4ad3469b63ba0ab5ce98c48e81840995
cynfonga <cynfonga@gmail.com>
2024-02-19 13:51:58 +0200

    init

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -5,3 +5,10 @@ span1.textContent= new Date().toLocaleDateString('en-Us',options);
 const span2= document.getElementById('lastmodification');
 
 span2.textContent= document.lastModified;
+
+function toggleMenu(){
+
+    document.getElementById('burgermenu').classList.toggle('open');
+}
+const button= document.getElementById('burgerbutton');
+button.onclick= toggleMenu;
