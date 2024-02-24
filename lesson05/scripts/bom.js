523e0507fde54babf72c8db0c88070aef5742d27
cynfonga <cynfonga@gmail.com>
2024-02-24 19:46:32 +0200

    init

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#favchap');
+
+const list= document.querySelector('#list');
+
+const button= document.querySelector('button');
+button.addEventListener('click', function(){
+
+const myItem= input.value;
+input.value="";
+
+const listItem=document.createElement('li');
+const listText=document.createElement('span');
+const listButton= document.createElement('button');
+
+listItem.appendChild(listText);
+listText.textContent=myItem
+listItem.appendChild(listButton);
+listButton.textContent="X";
+list.appendChild(listItem);
+
+listButton.addEventListener('click', function(){
+
+
+list.removeChild(ItemList)
+
+})
+
+input.focus();
+
+})
