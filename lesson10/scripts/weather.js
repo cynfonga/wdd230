5febc617acc71cf3c81b1ac357c4267159bf622e
cynfonga <cynfonga@gmail.com>
2024-04-01 05:20:13 +0200

    init

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,5 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=49.75&lon=6.64&appid=bbce11179c3c2746bd2abd14c887665e'
