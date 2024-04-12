f44b2cb9e7697b299c718701ef13a11e26156833
cynfonga <cynfonga@gmail.com>
2024-04-12 21:51:20 +0200

    init

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -31,6 +31,29 @@ if(numvisits!==0){
 
 
 
+  const pass1 = document.querySelector("#password1");
+const pass2 = document.querySelector("#password1");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (pass1.value !== pass2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		pass2.style.backgroundColor = "#fff0f3";
+		pass2.value = "";
+		pass1.focus();
+	} else {
+		message.style.display = "none";
+		pass2.style.backgroundColor = "#fff";
+		pass2.style.color = "#000";
+	}
+}
+
+
+
 
     
 
